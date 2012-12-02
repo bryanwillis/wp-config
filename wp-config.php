2cbca38fc777c66c5fa61416e7597d3037e6e5f0
Josué Cau <josuecau@yahoo.fr>
2012-12-02 16:04:57 +0100

    modif valeurs par defaut + phpdoc

--- wp-config.php
+++ wp-config.php
@@ -2,70 +2,70 @@
 
 /**
  * Set Database Name
- * @see http://codex.wordpress.org/Editing_wp-config.php#Set_Database_Name
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Set_Database_Name
  */
 define('DB_NAME', '');
 
 /**
  * Set Database User
- * @see http://codex.wordpress.org/Editing_wp-config.php#Set_Database_User
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Set_Database_User
  */
 define('DB_USER', '');
 
 /**
  * Set Database Password
- * @see http://codex.wordpress.org/Editing_wp-config.php#Set_Database_Password
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Set_Database_Password
  */
 define('DB_PASSWORD', '');
 
 /**
  * Set Database Host
- * @see http://codex.wordpress.org/Editing_wp-config.php#Set_Database_Host
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Set_Database_Host
  */
 define('DB_HOST', 'localhost');
 
 /**
  * Database character set
- * @see http://codex.wordpress.org/Editing_wp-config.php#Database_character_set
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Database_character_set
  */
 define('DB_CHARSET', 'utf8');
 
 /**
  * Database collation
- * @see http://codex.wordpress.org/Editing_wp-config.php#Database_collation
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Database_collation
  */
 define('DB_COLLATE', '');
 
 /**
  * Database Table prefix
- * @see http://codex.wordpress.org/Editing_wp-config.php#table_prefix
+ * @link http://codex.wordpress.org/Editing_wp-config.php#table_prefix
  */
 $table_prefix  = 'wp_';
 
 /**
  * Security Keys
- * @see http://codex.wordpress.org/Editing_wp-config.php#Security_Keys
- * @see https://api.wordpress.org/secret-key/1.1/salt/
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Security_Keys
+ * @link https://api.wordpress.org/secret-key/1.1/salt/
  */
-define('AUTH_KEY',         '%X<ID!jELTqb5,N$8EZq;_UkV6OuZ@$/pxQ0mc_t?+B.H_Pz-Xv3W1*`Om4<JKAD');
-define('SECURE_AUTH_KEY',  '#@ejJ;s@Z9^qgi]sz+e.m@koF,utxQCqo7@,fona7}i_n:alsZH~SR=2o=@{9(7a');
-define('LOGGED_IN_KEY',    '-DulA0v+p*<[~vUiV`+!d!=,BY)yqEO-T<~l|;5$=yCv}_:TZc; Dc_;Z||eY]&(');
-define('NONCE_KEY',        'm%tiTrp!ra/,I?:i5!RkHMjzSo^{=+.o$D_ky/S2y=z#Hyv20/nYA5{^9!Nqs Dp');
-define('AUTH_SALT',        '{-!>Fg|}kKz-7qM+kx|IO-MDAP0Y+V^_`ZAyb -I-HKP44g0Ubb;V{|&ZfWTuHd*');
-define('SECURE_AUTH_SALT', 'Tc-|G!VHFY@.>q-`;eh$(/k+;;ZnvUr:.`?*[FK$}}wb{Rmgz~]Eq>Yf}/d}]mGI');
-define('LOGGED_IN_SALT',   '58o8EradlB!;1P[/`F&`<K:MX<PlKzE`Ca6N|W+~-|%dt+,^^1I5k+Uj#(5sLNbF');
-define('NONCE_SALT',       'C.9J_S<6A-FE~H F(^~pxp(DEv`m1G<X-,h:+s?I58yZ>z0iW_x t*77;B8txtbR');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**
- * Language and Language Directory 
- * @see http://codex.wordpress.org/Editing_wp-config.php#Language_and_Language_Directory
+ * Language and Language Directory
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Language_and_Language_Directory
  */
 define('WPLANG', 'fr_FR');
 define('WP_LANG_DIR', dirname(__FILE__) . '/wp-content/languages');
 
 /**
  * Debug
- * @see http://codex.wordpress.org/Editing_wp-config.php#Debug
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Debug
  */
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', false);
@@ -74,68 +74,68 @@ define('SCRIPT_DEBUG', true);
 define('SAVEQUERIES', false);
 
 /**
- * Disable Javascript Concatenation
- * @see http://codex.wordpress.org/Editing_wp-config.php#Disable_Javascript_Concatenation
+ * Javascript Concatenation
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Disable_Javascript_Concatenation
  */
-define('CONCATENATE_SCRIPTS', false);
+define('CONCATENATE_SCRIPTS', true);
 
 /**
  * Increasing memory allocated to PHP
- * @see http://codex.wordpress.org/Editing_wp-config.php#Increasing_memory_allocated_to_PHP
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Increasing_memory_allocated_to_PHP
  */
 define('WP_MEMORY_LIMIT', '32M');
 
 /**
  * Enable Multisite / Network Ability
- * @see http://codex.wordpress.org/Editing_wp-config.php#Enable_Multisite_.2F_Network_Ability
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Enable_Multisite_.2F_Network_Ability
  */
-define('WP_ALLOW_MULTISITE', true);
+define('WP_ALLOW_MULTISITE', false);
 
 /** 
  * Define Site URL
- * @see http://codex.wordpress.org/Editing_wp-config.php#WordPress_address_.28URL.29
+ * @link http://codex.wordpress.org/Editing_wp-config.php#WordPress_address_.28URL.29
  */
-define('WP_HOME', $_SERVER['HTTP_HOST']);
-define('WP_SITEURL', $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', 'http://localhost');
+define('WP_HOME', 'http://localhost');
 
 /**
  * Moving wp-content 
- * @see http://codex.wordpress.org/Editing_wp-config.php#Moving_wp-content
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Moving_wp-content
  */
 define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
-define('WP_CONTENT_URL', $_SERVER['HTTP_HOST'] . '/wp-content');
+define('WP_CONTENT_URL', WP_SITEURL . '/wp-content');
 define('WP_PLUGIN_DIR', dirname(__FILE__) . '/wp-content/plugins');
-define('WP_PLUGIN_URL', $_SERVER['HTTP_HOST'] . '/wp-content/plugins');
+define('WP_PLUGIN_URL', WP_SITEURL . '/wp-content/plugins');
 define('PLUGINDIR', dirname(__FILE__) . '/wp-content/plugins');
 
 /**
  * Modify AutoSave Interval
- * @see http://codex.wordpress.org/Editing_wp-config.php#Modify_AutoSave_Interval
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Modify_AutoSave_Interval
  */
 define('AUTOSAVE_INTERVAL', 60);
 
 /**
  * Post Revisions
- * @see http://codex.wordpress.org/Editing_wp-config.php#Post_Revisions
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Post_Revisions
  */
 define('WP_POST_REVISIONS', true);
 define('WP_POST_REVISIONS', 5);
 
 /**
  * Set Cookie Domain
- * @see http://codex.wordpress.org/Editing_wp-config.php#Set_Cookie_Domain
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Set_Cookie_Domain
  */
-define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
+define('COOKIE_DOMAIN', 'localhost');
 
 /**
  * Cache
- * @see http://codex.wordpress.org/Editing_wp-config.php#Cache
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Cache
  */
-define('WP_CACHE', true);
+define('WP_CACHE', false);
 
 /**
  * Empty Trash
- * @see http://codex.wordpress.org/Editing_wp-config.php#Empty_Trash
+ * @link http://codex.wordpress.org/Editing_wp-config.php#Empty_Trash
  */
 define('EMPTY_TRASH_DAYS', 30);
 
